84a45a524772ff25723f322c8f583e061a5e93a7
Irina Shu <irina1125@yandex.ru>
2021-05-08 18:04:47 +0300

    Task1 done

--- task_1/solution.js
+++ task_1/solution.js
@@ -3,7 +3,14 @@ function addInCartMessage(title, price) {
     let productPrice = price;
 
     // Задание №1.1. Оповещение о добавлении в корзину
-
+    let message;
+    message=productName;
+    message=message+" ";
+    message=message+"за ";
+    message=message+productPrice;
+    message=message+" ";
+    message=message+"теперь в корзине!";
+    console.log(message);
     // создайте переменную message
     // присвойте переменной message значение переменной productName
     // добавьте в конец переменной message пробел
@@ -14,7 +21,7 @@ function addInCartMessage(title, price) {
     // выведите переменную message в консоль
 
     // Конец решения задания №1.1.
-
+    
     return message;
 }
 
@@ -26,6 +33,9 @@ function addInCartCountChange(value) {
     // создайте переменную newValue
     // присвойте ей значение oldValue
     // увеличьте значение newValue на 1
+let newValue;
+newValue=oldValue;
+newValue=newValue+1;
 
     // Конец решения задания №1.2.
 
@@ -37,7 +47,11 @@ function addInCartSumChange(sum, delta) {
     let difference = delta;
 
     // Задание №1.3. Увеличение суммы в корзине
-
+let newSum;
+newSum = oldSum+difference;
+let newSumText;
+newSumText=newSum+' ₽';
+console.log(newSum+' ₽');
     // создайте переменную newSum и присвойте ей сумму oldSum и difference
     // создайте переменную newSumText и присвойте ей результат склеивания переменной newSum и текста ' ₽'
     // замените склеивание, используемое в newSum Text на шаблонную строку
